8d8b09ac0720d17a9970da58f82c4446554d32ce
江小凡 <18373639691@163.com>
2019-03-15 10:15:23 +0800

    Site updated: 2019-03-15 10:15:18

--- js/src/algolia-search.js
+++ js/src/algolia-search.js
@@ -1,53 +1,50 @@
 /* global instantsearch, CONFIG */
-
-$(document).ready(function() {
+$(document).ready(function () {
   var algoliaSettings = CONFIG.algolia;
-  var isAlgoliaSettingsValid = algoliaSettings.applicationID
-                            && algoliaSettings.apiKey
-                            && algoliaSettings.indexName;
-
+  var isAlgoliaSettingsValid = algoliaSettings.applicationID &&
+    algoliaSettings.apiKey &&
+    algoliaSettings.indexName;
   if (!isAlgoliaSettingsValid) {
     window.console.error('Algolia Settings are invalid.');
     return;
   }
-
   var search = instantsearch({
-    appId         : algoliaSettings.applicationID,
-    apiKey        : algoliaSettings.apiKey,
-    indexName     : algoliaSettings.indexName,
-    searchFunction: function(helper) {
+    appId: algoliaSettings.applicationID,
+    apiKey: algoliaSettings.apiKey,
+    indexName: algoliaSettings.indexName,
+    searchFunction: function (helper) {
       var searchInput = $('#algolia-search-input').find('input');
-
       if (searchInput.val()) {
         helper.search();
       }
     }
   });
-
   // Registering Widgets
   [
     instantsearch.widgets.searchBox({
-      container  : '#algolia-search-input',
+      container: '#algolia-search-input',
       placeholder: algoliaSettings.labels.input_placeholder
     }),
 
     instantsearch.widgets.hits({
-      container  : '#algolia-hits',
+      container: '#algolia-hits',
       hitsPerPage: algoliaSettings.hits.per_page || 10,
-      templates  : {
-        item: function(data) {
-          var link = data.permalink ? data.permalink : CONFIG.root + data.path;
+      templates: {
+        item: function (data) {
+          var link = data.permalink ? data.permalink : CONFIG.root +
+            data.path;
           return (
-            '<a href="' + link + '" class="algolia-hit-item-link">'
-          + data._highlightResult.title.value
-          + '</a>'
+            '<a href="' + link + '" class="algolia-hit-item-link">' +
+            data._highlightResult.title.value +
+            '</a>'
           );
         },
-        empty: function(data) {
+        empty: function (data) {
           return (
-            '<div id="algolia-hits-empty">'
-          + algoliaSettings.labels.hits_empty.replace(/\$\{query}/, data.query)
-          + '</div>'
+            '<div id="algolia-hits-empty">' +
+            algoliaSettings.labels.hits_empty.replace(/\$\{query}/,
+              data.query) +
+            '</div>'
           );
         }
       },
@@ -59,56 +56,54 @@ $(document).ready(function() {
     instantsearch.widgets.stats({
       container: '#algolia-stats',
       templates: {
-        body: function(data) {
+        body: function (data) {
           var stats = algoliaSettings.labels.hits_stats
             .replace(/\$\{hits}/, data.nbHits)
             .replace(/\$\{time}/, data.processingTimeMS);
           return (
-            stats
-            + '<span class="algolia-powered">'
-            + '  <img src="' + CONFIG.root + 'images/algolia_logo.svg" alt="Algolia" />'
-            + '</span>'
-            + '<hr />'
+            stats +
+            '<span class="algolia-powered">' +
+            '  <img src="' + CONFIG.root +
+            'images/algolia_logo.svg" alt="Algolia" />' +
+            '</span>' +
+            '<hr />'
           );
         }
       }
     }),
 
     instantsearch.widgets.pagination({
-      container    : '#algolia-pagination',
-      scrollTo     : false,
+      container: '#algolia-pagination',
+      scrollTo: false,
       showFirstLast: false,
-      labels       : {
-        first   : '<i class="fa fa-angle-double-left"></i>',
-        last    : '<i class="fa fa-angle-double-right"></i>',
+      labels: {
+        first: '<i class="fa fa-angle-double-left"></i>',
+        last: '<i class="fa fa-angle-double-right"></i>',
         previous: '<i class="fa fa-angle-left"></i>',
-        next    : '<i class="fa fa-angle-right"></i>'
+        next: '<i class="fa fa-angle-right"></i>'
       },
       cssClasses: {
-        root    : 'pagination',
-        item    : 'pagination-item',
-        link    : 'page-number',
-        active  : 'current',
+        root: 'pagination',
+        item: 'pagination-item',
+        link: 'page-number',
+        active: 'current',
         disabled: 'disabled-item'
       }
     })
   ].forEach(search.addWidget, search);
-
   search.start();
-
-  $('.popup-trigger').on('click', function(e) {
+  $('.popup-trigger').on('click', function (e) {
     e.stopPropagation();
     $('body')
-      .append('<div class="search-popup-overlay algolia-pop-overlay"></div>')
+      .append(
+        '<div class="search-popup-overlay algolia-pop-overlay"></div>')
       .css('overflow', 'hidden');
     $('.popup').toggle();
     $('#algolia-search-input').find('input').focus();
   });
-
-  $('.popup-btn-close').click(function() {
+  $('.popup-btn-close').click(function () {
     $('.popup').hide();
     $('.algolia-pop-overlay').remove();
     $('body').css('overflow', '');
   });
-
 });
--- js/src/schemes/muse.js
+++ js/src/schemes/muse.js
@@ -1,7 +1,13 @@
-$(document).ready(function() {
+$(document).ready(function () {
   function updateFooterPosition() {
-    var containerHeight = $('#footer').attr('position') ? $('.container').height() + $('#footer').outerHeight(true) : $('.container').height();
-    if (containerHeight < window.innerHeight) $('#footer').css({ 'position': 'fixed', 'bottom': 0, 'left': 0, 'right': 0 }).attr('position', 'fixed');
+    var containerHeight = $('#footer').attr('position') ? $('.container')
+      .height() + $('#footer').outerHeight(true) : $('.container').height();
+    if (containerHeight < window.innerHeight) $('#footer').css({
+      'position': 'fixed',
+      'bottom': 0,
+      'left': 0,
+      'right': 0
+    }).attr('position', 'fixed');
     else $('#footer').removeAttr('style position');
   }
   updateFooterPosition();
