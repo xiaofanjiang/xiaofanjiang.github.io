6d86d6079b33b12f760e28ca20daca641508f5eb
江小凡 <18373639691@163.com>
2019-06-29 12:20:56 +0800

    Site updated: 2019-06-29 12:20:50

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,5 +1,7 @@
 /* global NexT, CONFIG */
-$(document).ready(function () {
+
+$(document).ready(function() {
+
   var sidebarInner = $('.sidebar-inner');
   var sidebarOffset = CONFIG.sidebar.offset || 12;
 
@@ -18,40 +20,38 @@ $(document).ready(function () {
   function initAffix() {
     var headerOffset = getHeaderOffset();
     var footerOffset = getFooterOffset();
-    var sidebarHeight = $('#sidebar').height() + NexT.utils
-      .getSidebarb2tHeight();
+    var sidebarHeight = $('#sidebar').height() + NexT.utils.getSidebarb2tHeight();
     var contentHeight = $('#content').height();
+
     // Not affix if sidebar taller than content (to prevent bottom jumping).
     if (headerOffset + sidebarHeight < contentHeight) {
       sidebarInner.affix({
         offset: {
-          top: headerOffset - sidebarOffset,
+          top   : headerOffset - sidebarOffset,
           bottom: footerOffset
         }
       });
       sidebarInner.affix('checkPosition');
     }
-    $('#sidebar').css({
-      'margin-top': headerOffset,
-      'margin-left': 'auto'
-    });
+
+    $('#sidebar').css({ 'margin-top': headerOffset, 'margin-left': 'auto' });
   }
 
   function recalculateAffixPosition() {
     $(window).off('.affix');
-    sidebarInner.removeData('bs.affix').removeClass(
-      'affix affix-top affix-bottom');
+    sidebarInner.removeData('bs.affix').removeClass('affix affix-top affix-bottom');
     initAffix();
   }
 
   function resizeListener() {
     var mql = window.matchMedia('(min-width: 992px)');
-    mql.addListener(function (e) {
+    mql.addListener(function(e) {
       if (e.matches) {
         recalculateAffixPosition();
       }
     });
   }
+
   initAffix();
   resizeListener();
 });
